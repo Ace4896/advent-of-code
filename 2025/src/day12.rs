d67844b6e67a47716b7335d568c226559130e499
Jon Pacheco <jon.pacheco2@gmail.com>
2025-12-11 11:38:14 +0000

    chore: setup day 12

---
+++ 2025/src/day12.rs
@@ -0,0 +1,19 @@
+use std::{env, process};
+
+use advent_of_code_2025::*;
+
+fn main() {
+    println!("----- Day 12 -----");
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("No input file specified");
+        process::exit(1);
+    }
+
+    let input_path = &args[1];
+    println!("Input File: {}", input_path);
+
+    // println!("Part 1: {}", solve_part_1(input_path));
+    // println!("Part 2: {}", solve_part_2(input_path));
+}
